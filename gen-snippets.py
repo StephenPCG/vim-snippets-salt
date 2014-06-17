ada30e8a1d0f141066e359fb6daee75dfe24d108
Zhang Cheng <StephenPCG@gmail.com>
2014-06-17 22:01:09 +0800

    add arg support
    * -i, --ignore-functions: ignore functions to be included in snippet
    * -p, --salt-path: path to salt source
    * -e, --engine: snippet engine, only neosnippet is supported currently

--- gen-snippets.py
+++ gen-snippets.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import glob
 import inspect
+import argparse
 import importlib
 
 class NoDefault():
@@ -27,7 +29,7 @@ def list_states_modules():
         module = importlib.import_module("salt.states." + module_name)
         yield module
 
-def list_module_funcs(module):
+def list_module_funcs(module, ignore_functions=[]):
     """ Scan for all states functions in a module.
 
     Returns a list of tupple:
@@ -35,7 +37,8 @@ def list_module_funcs(module):
     """
     module_name = module.__name__.replace("salt.states.", "")
     for (name, member) in inspect.getmembers(module):
-        if inspect.isfunction(member) and not name.startswith("_"):
+        if inspect.isfunction(member) and not name.startswith("_") \
+                and name not in ignore_functions:
 
             args, _, _, defaults = inspect.getargspec(member)
 
@@ -62,13 +65,39 @@ def gen_snippet(module_name, function_name, argspec):
         if isinstance(default, NoDefault):
             default = ''
         lines.append("    - %s: ${%s:%s}" % (arg, idx, default))
+    lines.append("")
 
     return "\n".join(lines)
 
+def parseargs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--engine", default="neosnippet",
+            help="snippet engine: neosnippet, ultisnips")
+    parser.add_argument("-i", "--ignore-function", default=[],
+            action="append", help="ignore functions, may be repeated")
+    parser.add_argument("-p", "--salt-path", default=None,
+            help="salt source path")
+
+    return parser.parse_args(sys.argv[1:])
+
 def main():
-    for module in list_states_modules():
-        for module_name, function_name, argspec in list_module_funcs(module):
-            print gen_snippet(module_name, function_name, argspec)
+    args = parseargs()
+
+    if args.engine != "neosnippet":
+        print "Only 'neosnippet' is supported currently."
+        sys.exit(0)
+
+    if args.salt_path is not None:
+        sys.path.insert(0, args.salt_path)
+
+    salt_version = get_salt_version()
+    output_path = os.path.join(os.path.dirname(__file__), "snippets")
+    output_file = os.path.join(output_path, "sls-%s.snippets" % salt_version)
+
+    with open(output_file, "w+") as fp:
+        for module in list_states_modules():
+            for module_name, function_name, argspec in list_module_funcs(module):
+                fp.write(gen_snippet(module_name, function_name, argspec))
 
 if __name__ == '__main__':
     main()
